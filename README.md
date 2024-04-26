56f4ba7f275a9a54f807acd822b18982f2c4e7e0
Ben Serrano <bserrano8970@gmail.com>
2024-04-25 22:28:13 -0500

    Add README and foot config - update emacs colorscheme

---
+++ README.md
@@ -0,0 +1,10 @@
+# My Dotfiles
+Updated my dotfiles to use both Emacs and Neovim as they've both grown on me.
+
+Font in use currently: [BlexMono Nerd Font Text](https://www.nerdfonts.com/font-downloads) - [Download](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/IBMPlexMono.zip)
+Current Terminal: Foot
+Editors:
+1. Emacs
+    - Using Emacs from Fedora with customizations for tree sitter and eglot soon
+1. Neovim
+    - Using Neovim from Fedora with customzations for lspconfig, and telescope
--- emacs/init.el
+++ emacs/init.el
@@ -15,10 +15,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(doom-challenger-deep))
+ '(custom-enabled-themes '(doom-tokyo-night))
  '(custom-safe-themes
-   '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" default))
- '(package-selected-packages '(doom-themes powerline)))
+   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1" "4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "b5fd9c7429d52190235f2383e47d340d7ff769f141cd8f9e7a4629a81abc6b19" default))
+ '(package-selected-packages '(company doom-themes powerline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
